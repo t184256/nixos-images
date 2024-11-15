7d0e638e0412a4f154bcabc8faf35558fad63dde
Lucas Romero <luqasn@gmail.com>
2024-11-15 17:46:25 +0100

    Enable zRamSwap to allow running on low-mem systems

--- nix/kexec-installer/module.nix
+++ nix/kexec-installer/module.nix
@@ -33,6 +33,9 @@ in
   };
 
   config = {
+    zramSwap = {
+      enable = true;
+    };
     boot.initrd.compressor = "xz";
     # This is a variant of the upstream kexecScript that also allows embedding
     # a ssh key.
